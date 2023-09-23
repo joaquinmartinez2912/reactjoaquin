c318f059b1b46dc68acba0013b132e572ce098de
Joaquin Martinez <jo.martinez@itecriocuarto.org.ar>
2023-09-23 12:06:10 -0300

    sabado

--- src/components/ListaMenu.jsx
+++ src/components/ListaMenu.jsx
@@ -3,36 +3,39 @@ import "../main.css";
 
 function ListaMenu(props) {
   const datos = props.lista
+  
 
   // Defino una variable con un estado para controlar el submenu que contiene una lista con los datos a mostrar.
+  const [estadoSubLista, setestadoSubLista] = useState(false)
   const [mostrarSubLista, setMosrarSubLista] = useState([])
-  // const [left, setleft] = useState(0)
-  // const [top, settop] = useState(0)
-  // const [veces, setVeces] = useState(0)
+
+  const [left, setleft] = useState(0)
+  const [top, settop] = useState(0)
+  const [veces, setVeces] = useState(0)
 
   //Defino los manejadores de eventos para mostrar el submenu, no cargo datos con esto, solo abro el submenu
   const handleMostrarSubMenu = (SubMenuId, e) => {
+    console.log( e)
     e.preventDefault();
 
-    // const { offsetLeft, offsetTop, offsetWidth } = e.target;
-    // setleft(`${offsetLeft + offsetWidth}px`);
-    // settop(`${offsetTop}px`);
+    const { offsetLeft, offsetTop, offsetWidth, clientHeight, clientLeft, clientTop, clientWidth } = e.target;
+    setleft(`${clientLeft}px`);
+    settop(`${clientHeight}px`);
+    console.log(clientHeight)
+    console.log(clientLeft)
+    console.log(clientTop)
+    console.log(clientWidth)
+
+    setestadoSubLista(!estadoSubLista)
+    console.log( estadoSubLista)
 
     setMosrarSubLista((prev) => {
       const arr = [...prev];
       arr[SubMenuId] = true; // Que esto sea verdadero o falso define como se va a completar el css de la <ul>
-      // setVeces(veces+1)
-      // alert(veces)
-      return arr;
-    });
-  };
+      //setVeces(veces+1)
+      //alert(veces)
+      console.log(arr)
 
-  const handleOcultarSubMenu = (SubMenuId, e) => {
-    e.preventDefault();
-
-    setMosrarSubLista((prev) => {
-      const arr = [...prev];
-      arr[SubMenuId] = false; // Que esto sea verdadero o falso define como se va a completar el css de la <ul>
       return arr;
     });
   };
@@ -48,22 +51,21 @@ function ListaMenu(props) {
                 // Capturo el evento (Hacer click en un item determinado) y mando el id que corresponde para que se ejecute la funcion. El id lo uso para modificar el estado
                 key={item.id}
                 onClick={(e) => handleMostrarSubMenu(item.id, e)}
-                //onMouseLeave={(e) => handleOcultarSubMenu(item.id, e)}
-              >
-                <div>
-                  <span>{item.name}</span>
+                >
+                <div style={{position: "absolute"}}>
+                  <span>{item.name +"(flecha)"}</span>
                 </div>
                 {/* Agrego la ul que se va a mostrar cuando se haga click */}
                 <ul className={`sub-menu-ul ${mostrarSubLista[item.id] ? "open" : ""}`}
-                  // style={{
-                  //   left: left,
-                  //   top: top
-                  // }}
+                  style={{
+                    left: left,
+                    top: top
+                  }}
                 >
                   {
                     item.list.map((elemento) => {
                       return (
-                        <li style={{ backgroundColor: "green" }} > {elemento.name} </li>
+                        <li key={elemento.id} style={{ backgroundColor: "green" }} > {elemento.name} </li>
                       )
                     })
                   }
--- src/components/a.jsx
+++ src/components/a.jsx
@@ -3,12 +3,13 @@ import "../main.css";
 
 function ListaMenu(props) {
   const datos = props.lista
+  
 
   // Defino una variable con un estado para controlar el submenu que contiene una lista con los datos a mostrar.
   const [mostrarSubLista, setMosrarSubLista] = useState([])
   const [left, setleft] = useState(0)
   const [top, settop] = useState(0)
-  // const [veces, setVeces] = useState(0)
+  //const [veces, setVeces] = useState(0)
 
   //Defino los manejadores de eventos para mostrar el submenu, no cargo datos con esto, solo abro el submenu
   const handleMostrarSubMenu = (SubMenuId, e) => {
@@ -21,8 +22,8 @@ function ListaMenu(props) {
     setMosrarSubLista((prev) => {
       const arr = [...prev];
       arr[SubMenuId] = true; // Que esto sea verdadero o falso define como se va a completar el css de la <ul>
-      // setVeces(veces+1)
-      // alert(veces)
+      //setVeces(veces+1)
+      //alert(veces)
       return arr;
     });
   };
@@ -50,20 +51,20 @@ function ListaMenu(props) {
                 onMouseEnter={(e) => handleMostrarSubMenu(item.id, e)}
                 onMouseLeave={(e) => handleOcultarSubMenu(item.id, e)}
               >
-                <div>
-                  <span>{item.name}</span>
+                <div style={{position: "absolute"}}>
+                  <span>{item.name +"(flecha)"}</span>
                 </div>
                 {/* Agrego la ul que se va a mostrar cuando se haga click */}
                 <ul className={`sub-menu-ul ${mostrarSubLista[item.id] ? "open" : ""}`}
-                  // style={{
-                  //   left: left,
-                  //   top: top
-                  // }}
+                  style={{
+                     left: left,
+                     top: top
+                   }}
                 >
                   {
                     item.list.map((elemento) => {
                       return (
-                        <li style={{ backgroundColor: "green" }} > {elemento.name} </li>
+                        <li key={elemento.id} style={{ backgroundColor: "green" }} > {elemento.name} </li>
                       )
                     })
                   }
