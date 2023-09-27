d1748584642e66d1765c521c37ce134fabd2b816
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2023-09-27 12:02:22 -0300

    colores y flecha

--- src/components/ListaMenu.jsx
+++ src/components/ListaMenu.jsx
@@ -15,93 +15,111 @@ function ListaMenu(props) {
     e.preventDefault();
 
     setestadoSubLista(!estadoSubLista)
-    estadoSubLista2 && 
-    setestadoSubLista2(false)
-  ;
+    estadoSubLista2 &&
+      setestadoSubLista2(false)
+      ;
   };
 
   const handleSubMenuNivel3 = (e) => {
     e.preventDefault();
     setestadoSubLista2(!estadoSubLista2)
 
-    ;
+      ;
   };
 
   return (
 
-    <ul className="main-nav" >
+    <ul className="main-nav" style={
+      {
+        background: datos.configColor.background,
+      }
+    } >
 
       {datos.menuItems.map((item) => {
         return (
           <div>
-          {item.idPadre === datos.idFirstNivel ?
-            item.isFolder ? <li
-              key={item.id}
-            >
-                <span onClick={(e) => handleMostrarSubMenu(e)}>{item.name}</span>
-              
-                {estadoSubLista &&  
-                <ul className="ul-segundo-nivel">
-                  
-                  {datos.menuItems.map((subItem) => {
-                    return (
-                      item.id === subItem.idPadre ?
-                      subItem.isFolder ?
-                        <li
-                        key={subItem.id}
-                        onMouseEnter={(e) => handleSubMenuNivel3(e)}
-                        onMouseLeave={(e) => handleSubMenuNivel3(e)}
-                        >
-                          <span>{subItem.name}</span>
-                        
-                          {estadoSubLista2 &&
-                          <ul className="ul-tercer-nivel">
-                            {datos.menuItems.map((subItem3) => {
-                              return (
-                                subItem.id === subItem3.idPadre &&
-                                <li>{subItem3.name}</li>
-                              )
-                            }
-                            )}
-                          </ul>
-                          }
-                        </li>
-                      :
-                      <li
-                      key={subItem.id}
-                      >
-                        {subItem.name}
-                      </li>
-                      :
-                      ""
-                    )
-                  })
-                  }
-
-                </ul>}
-              
-
-            </li>
-
-              :
-              <li
-              key={item.id}
+            {item.idPadre === datos.idFirstNivel ?
+              item.isFolder ? <li
+                className="li-Menu"
+                key={item.id}
+                style={{
+                  background: datos.configColor.itemBackground,
+                  color: estadoSubLista ? datos.configColor.itemActive : datos.configColor.itemColor
+                }
+                }
               >
+                <span onClick={(e) => handleMostrarSubMenu(e)}>{item.name}<svg xmlns="http://www.w3.org/2000/svg" width={"20px"} height={"15px"} fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4 4a.5.5 0 0 0-.374.832l4 4.5a.5.5 0 0 0 .748 0l4-4.5A.5.5 0 0 0 12 6H4z" />
+                </svg></span>
 
-                {item.name}
+                {estadoSubLista &&
+                  <ul className="ul-segundo-nivel">
+                    {datos.menuItems.map((subItem) => {
+                      return (
+                        item.id === subItem.idPadre ?
+                          subItem.isFolder ?
+                            <li
+                              key={subItem.id}
+                              className="li-subMenu"
+                              style={{
+                                background: datos.configColor.itemBackground,
+                                color: estadoSubLista2 && datos.configColor.itemActive
+                              }}
+                              onMouseEnter={(e) => handleSubMenuNivel3(e)}
+                              onMouseLeave={(e) => handleSubMenuNivel3(e)}
+                            >
+                              <span>{subItem.name}<svg xmlns="http://www.w3.org/2000/svg" width={"20px"} height={"15px"} fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z" />
+                              </svg></span>
+
+                              {estadoSubLista2 &&
+                                <ul className="ul-tercer-nivel">
+                                  {datos.menuItems.map((subItem3) => {
+                                    return (
+                                      subItem.id === subItem3.idPadre &&
+                                      <li key={subItem3.id} className="li-subMenu">{subItem3.name}</li>
+                                    )
+                                  }
+                                  )}
+                                </ul>
+                              }
+                            </li>
+                            :
+                            <li
+                              key={subItem.id}
+                              className="li-subMenu"
+                            >
+                              {subItem.name}
+                            </li>
+                          :
+                          ""
+                      )
+                    })
+                    }
+
+                  </ul>}
+
 
               </li>
-            :
-            ""}
-            </div>
+
+                : // Este es el negativo del menu nivel 1. Lo que pasa si isfolder = false 
+                <li
+                  key={item.id}
+                  className="li-Menu"
+                  style={{ color: datos.configColor.itemColor }}
+                >
+
+                  {item.name}
+
+                </li>
+              :
+              ""}
+          </div>
         )
-        
+
       })
       }
-
     </ul>
-
-
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,29 +1,27 @@
 import React, { useState } from 'react'
-import data from "../data/items.json";
+//import data from "../data/items.json";
 import "../main.css";
 import ListaMenu from './ListaMenu';
+
 
 function Menu() {
 
   const configMenu = {
     configColor: {
-      background: '#f4f5fa', // Color de Fondo General de la botonera
+      background:'#f4f5fa', // Color de Fondo General de la botonera
       itemBackground: '#d0d0d0',// Color de Fondo de los subMenús
       itemColor: '#666', // Color del texto de cada item del menú
-      itemActive: '#a8a8a8', // Color cuando hace click y se abre un  submenú
+      itemActive: 'blue', // Color cuando hace click y se abre un  submenú '#a8a8a8',
     },
     idFirstNivel: 150,
     menuItems: [
       { name: 'Another Action Nivel1', isFolder: false, id: 148, idPadre: 150 },
-      { name: 'agregado Nivel1', isFolder: false, id: 149, idPadre: 150 },
       { name: 'sub menu Nivel1', isFolder: true, id: 2, idPadre: 150 },
       { name: 'Action Nivel2', isFolder: false, id: 3, idPadre: 2 },
       { name: 'Another action Nivel2', isFolder: false, id: 4, idPadre: 2 },
       { name: 'sub menu Nivel2', isFolder: true, id: 5, idPadre: 2 },
-      { name: 'agregado Nivel2', isFolder: false, id: 44, idPadre: 2 },
       { name: 'Another action Nivel3', isFolder: false, id: 55, idPadre: 5 },
       { name: 'something else here Nivel3', isFolder: false, id: 56, idPadre: 5 },
-      { name: 'agregado Nivel3', isFolder: false, id: 57, idPadre: 5 },
     ],
   }
 
@@ -36,14 +34,9 @@ function Menu() {
   return (
     <div>
       <button onClick={handlerMenu} >Menu</button>
-      <nav style={{background: configMenu.configColor.background}}>
-
-
         {mostrarMenu &&
           <ListaMenu data={configMenu} />
         }
-
-      </nav>
     </div>
   )
 }
