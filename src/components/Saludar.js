1836490ca5db614bdfbe5419ad42c7ec68c1dc68
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2023-09-28 15:16:32 -0300

    limpieza de repositorio

--- src/components/Saludar.js
+++
@@ -1,8 +0,0 @@
-import './Saludar.css'
-
-function Saludar(props){
-    const {nombre, apellido="valor por defecto"} = props
-    return <p className='p-saludar'> Bienvenido <b>{nombre} {apellido}</b></p>
-  }
-export default Saludar
-
